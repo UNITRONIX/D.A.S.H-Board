d953d31ce947062e78becefbaa71298ff09a0a13
UNITRONIX <36471318+UNITRONIX@users.noreply.github.com>
2025-09-12 06:34:15 +0200

    startup_sh update

--- start_dash.sh
+++ start_dash.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+sudo apt-get install python3-dev
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade setuptools
+sudo pip3 install --upgrade wheel
+sudo pip3 install -r requirements.txt
 sudo python3 main.py
